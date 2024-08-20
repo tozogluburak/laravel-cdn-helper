8193e2c4b3aba33389d79617d404f86f6e8590f2
Burak <tozogluburak@gmail.com>
2024-08-20 23:36:59 +0300

    upgrade v1.1

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace tozogluburak\CdnHelper;
 
 class ComposerScripts
@@ -7,12 +6,32 @@ class ComposerScripts
     public static function updateAutoload()
     {
         // Laravel basepath()
-        $basePath = dirname(__DIR__, 4); 
+        $basePath = dirname(__DIR__, 4);
+
+        $helpersDir = $basePath . '/app/Helpers';
+        if (!is_dir($helpersDir)) {
+            mkdir($helpersDir, 0755, true);
+        }
+
+        // Copy CdnHelpers.php from vendor to app/Helpers
+        $source = __DIR__ . '/Helpers/CdnHelpers.php';
+        $destination = $helpersDir . '/CdnHelpers.php';
+
+        if (file_exists($source)) {
+            copy($source, $destination);
+        }
+
+        // Update composer autoload files section
+        self::updateComposerAutoload($basePath);
+    }
+
+    private static function updateComposerAutoload($basePath)
+    {
         $composerJsonPath = $basePath . '/composer.json';
 
         $composerJson = json_decode(file_get_contents($composerJsonPath), true);
 
-        // Add or update filespace if available
+        // Add or update autoload files section
         if (!isset($composerJson['autoload']['files'])) {
             $composerJson['autoload']['files'] = [];
         }
@@ -22,10 +41,10 @@ class ComposerScripts
             $composerJson['autoload']['files'][] = $cdnHelperPath;
         }
 
-        // Save the JSON file again
+        // Save the updated composer.json file
         file_put_contents($composerJsonPath, json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
-        // rebuild autoload by running composer dump-autoload
+        // Rebuild autoload
         exec('composer dump-autoload');
     }
 }
