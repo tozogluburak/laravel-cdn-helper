e11c56b75d0c20baf0a71bc5480a57cb27a34d53
Burak <tozogluburak@gmail.com>
2024-08-21 05:12:46 +0300

    upgrade v1.7.0

--- src/Discovery/CdnHelperDiscovery.php
+++ src/Discovery/CdnHelperDiscovery.php
@@ -4,20 +4,19 @@ namespace tozogluburak\CdnHelper\Discovery;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Foundation\PackageManifest;
+
 
 class CdnHelperDiscovery extends ServiceProvider
 {
     public function boot(Filesystem $filesystem)
     {
+        // Publish the CdnHelpers.php file to the app/Http/Helpers directory
         $this->publishes([
             __DIR__.'/../Helpers/CdnHelpers.php' => app_path('Http/Helpers/CdnHelpers.php'),
         ]);
-
-        // Get the package manifest instance
-        $manifest = new PackageManifest($filesystem);
-
-        // Register the package
-        $manifest->packages()->push('tozogluburak/cdn-helper');
+    }
+    public function register()
+    {
+        //
     }
 }
